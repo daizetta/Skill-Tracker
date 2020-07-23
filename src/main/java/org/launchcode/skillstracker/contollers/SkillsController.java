75c81d6059547f12a8dedcf7b56ebd7ea217fb33
daizetta <daizettaw@gmail.com>
2020-07-23 10:10:59 -0400

    Completed studio 10

--- src/main/java/org/launchcode/skillstracker/contollers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/contollers/SkillsController.java
@@ -71,7 +71,7 @@ public class SkillsController {
                         "<h1>Skills Tracker for " + username + "</h1>" +
                         "<h2>Skills to learn</h2>" +
                         "<ol>" +
-                        "<li>"+ irstSkill + "</li>" +
+                        "<li>"+ firstSkill + "</li>" +
                         "<li>"+ secondSkill + "</li>" +
                         "<li>"+ thirdSkill+ "</li>" +
                         "</ol>" +
